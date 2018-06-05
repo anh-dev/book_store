03440f9dd5cd67f5a5d1594a5331596ac30c881d
Andrii Holub <andr.holub@gmail.com>
2018-06-05 11:06:42 +0300

    rest tests

--- src/main/java/com/pluralsight/bookstore/util/IsbnGenerator.java
+++ src/main/java/com/pluralsight/bookstore/util/IsbnGenerator.java
@@ -5,6 +5,6 @@ import java.util.Random;
 public class IsbnGenerator implements NumberGenerator {
     @Override
     public String generateNumber() {
-        return "13-5677-" + Math.abs(new Random().nextInt());
+        return "13-84356-" + Math.abs(new Random().nextInt());
     }
 }
